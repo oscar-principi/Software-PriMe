c831f89bcfdc5ad8a2531ad13b404e7b718b84cb
oscar-principi <principioscar89gmail.com>
2025-10-27 19:23:39 -0300

    Primer commit con el proyecto estable

---
+++ PortafolioWeb/wwwroot/js/scroll.js
@@ -0,0 +1,6 @@
+﻿window.scrollToElement = (elementId) => {
+    const el = document.getElementById(elementId);
+    if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+};
